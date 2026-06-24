906b88104e92cf193787a2670ab6710498c9ad57
Howard <howard.zhang0108@gmail.com>
2026-06-23 22:10:41 -0400

    Add styling to grid boxes and container
    Made the container a flex box and the grids flex
    items with wrap so that they fill out the
    container. Also set the grids to border-box
    box-sizing so that the border doesn't get factored
    into the overall size. This way sizing is kept
    consistent at 40x40px.

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ body.appendChild(container);
 container.style.width = "640px";
 container.style.height = "640px";
 container.style.backgroundColor = "red";
+container.setAttribute("class", "container");
 
 for (let i = 0; i < 256; i++) {
     const grid = document.createElement("div");
     grid.style.width = "40px";
     grid.style.height = "40px";
     grid.style.backgroundColor = "blue";
+    grid.style.border = "1px solid black";
+    grid.setAttribute("class", "grid");
     container.appendChild(grid);
 }
