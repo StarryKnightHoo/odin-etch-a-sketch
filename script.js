2e6b379dce39c3f94e40dca98026e504fa345c30
Howard <howard.zhang0108@gmail.com>
2026-06-23 21:42:15 -0400

    Add grid boxes
    Used a for loop to create all the individual grid
    boxes. Temporarily also colored the container and
    grid boxes so that I can see they actually worked.

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let body = document.body;
 let container = document.createElement("div");
 
 body.appendChild(container);
+
+container.style.width = "640px";
+container.style.height = "640px";
+container.style.backgroundColor = "red";
+
+for (let i = 0; i < 256; i++) {
+    const grid = document.createElement("div");
+    grid.style.width = "40px";
+    grid.style.height = "40px";
+    grid.style.backgroundColor = "blue";
+    container.appendChild(grid);
+}
