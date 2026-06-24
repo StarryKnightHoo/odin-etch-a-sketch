47a0fe95c77583c6f5e0e8a42f92e79c6a1f91fd
Howard <howard.zhang0108@gmail.com>
2026-06-24 17:34:22 -0400

    Add button click event
    Clicking new grid now successfully asks for a new
    grid size that will also re-ask if inputted over
    100. The old grid will then be removed and
    replaced by the new grid.

--- script.js
+++ script.js
@@ -30,3 +30,28 @@ for (let i = 0; i < (gridPerSide * gridPerSide); i++) {
         grid.style.backgroundColor = "purple";
     });
 }
+
+btn.addEventListener("click", () => {
+    container.innerHTML = "";
+    
+    let input = prompt("Enter grid size: ");
+    if (input > 100) {
+        gridPerSide = prompt("Please enter a smaller number: ");
+    } else {
+        gridPerSide = input;
+    }
+
+    for (let i = 0; i < (gridPerSide * gridPerSide); i++) {
+        const grid = document.createElement("div");
+    grid.style.width = `${640 / gridPerSide}px`;
+    grid.style.height = `${640 / gridPerSide}px`;
+    grid.style.backgroundColor = "grey";
+    grid.style.border = "1px solid black";
+    grid.setAttribute("class", "grid");
+    container.appendChild(grid);
+    grid.addEventListener("mouseenter", () => {
+        grid.style.backgroundColor = "purple";
+    })
+    }
+
+});
